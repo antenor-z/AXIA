6693f17457a48f3e29bac5e798a1fc02426e46a0
antenor-z <antenor-z@pm.me>
2024-02-02 10:08:17 -0300

    showing file to be renamed

--- static/renameModal.js
+++ static/renameModal.js
@@ -1,11 +1,16 @@
 var modalRename = document.getElementById('modalRename');
 modalRename.style.display = 'none';
-function modalRenameClick(event, a) {
-    formRename = document.getElementById('form-rename')
-    formRename.action = a
+function modalRenameClick(event, a = null) {
     event.preventDefault()
+    event.stopPropagation()
     if (modalRename.style.display === 'none') {
         modalRename.style.display = 'block';
+        const formRename = document.getElementById('form-rename')
+        formRename.action = a
+        const fileRename = document.getElementById('file-rename')
+        const parts = a.split("/")
+        const fileName = parts[parts.length - 1]
+        fileRename.innerText = fileName
     } else {
         modalRename.style.display = 'none';
     }
