a7cb348d5ad5f862bdae3eb13d23136ef2a9dacb
arm <arm@stargate>
2024-02-10 11:12:06 -0300

    clear tooltip if clicked

--- static/toggleMore.js
+++ static/toggleMore.js
@@ -8,3 +8,12 @@ function toggleMore(event, filePath) {
         element.style.display = 'none'
     }
 }
+function toggleMoreNoPrevent(event, filePath) {
+    event.stopPropagation()
+    element = document.getElementById('more-' + filePath)
+    if (element.style.display === 'none') {
+        element.style.display = ''
+    } else {
+        element.style.display = 'none'
+    }
+}
