b4dbb6651dc8812919d1363ced3192c2bea020bb
Antenor Leal <antenor.leal@oceanpact.com>
2024-02-15 17:36:57 -0300

    hold line hover color when mouse away from context menu

--- static/contextMenu.js
+++ static/contextMenu.js
@@ -1,9 +1,15 @@
 function closeAllContextMenus() {
     const tooltips = document.querySelectorAll(".tooltip")
     tooltips.forEach(tooltip => {tooltip.style.display = 'none'} )
+    const selectedLines = document.querySelectorAll(".hold-select-color")
+    console.log(selectedLines)
+    selectedLines.forEach(line => {
+        line.classList.remove('hold-select-color');
+    })
 }
 
 function showContextMenu(event, filePath) {
+    closeAllContextMenus()
     event.preventDefault() // Prevent the default browser context menu
     event.stopPropagation()
 
@@ -12,11 +18,14 @@ function showContextMenu(event, filePath) {
 
     if (x < 300) { x = 300 }
 
-    const element = document.getElementById('more-' + filePath)
+    const element = document.getElementById('content-menu-' + filePath)
     element.style.left = x + 'px'
     element.style.top = y + 'px'
     
     element.style.display = 'grid'
+
+    const line = document.getElementById('tr-' + filePath)
+    line.classList.add('hold-select-color')
 }
 
 document.addEventListener("click", closeAllContextMenus);
